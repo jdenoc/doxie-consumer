0702b769857e25ac487c5bc31eb3f06ac15d3345
jdenoc <jdenoc@gmail.com>
2016-12-20 18:30:36 -0600

    per instructions from doxie support, we're calling the list_scans() and get_thumbnail() before we call get_scan() in order to keep the scanner "awake".

--- src/consumer.php
+++ src/consumer.php
@@ -56,7 +56,24 @@ if(!$doxie_consumer->is_available()){
 $obtained_scans = array();
 $doxie_scans = $doxie_consumer->list_scans();
 foreach($doxie_scans as $doxie_scan){
-    $downloaded = $doxie_consumer->get_scan($doxie_scan);
+    // there's a bug that downloading scans won't reset the scanner's auto-off timer.
+    // You can work around it by occasionally getting a list of scans via the API while you're downloading,
+    // which will reset the auto-off timer. -Doxie Support [2016-12-15]
+    $doxie_consumer->list_scans();
+
+    // The scanner should't fall asleep when calling the scan.json command, but it is.
+    // In the meantime the best solution would be to fetch the thumbnail before transferring each scan,
+    // even if you don't end up using the thumbnail for anything. - Doxie Support [2016-12-20]
+    $doxie_consumer->get_thumbnail(
+        $doxie_scan,
+        sys_get_temp_dir().DIRECTORY_SEPARATOR.'thumbnail_'.$doxie_consumer->generate_download_filename($doxie_scan)
+    );
+
+    // Actually download the scan from the Doxie Scanner
+    $downloaded = $doxie_consumer->get_scan(
+        $doxie_scan,
+        $doxie_consumer->get_download_location().DIRECTORY_SEPARATOR.$doxie_consumer->generate_download_filename($doxie_scan)
+    );
     if($downloaded){
         $obtained_scans[] = $doxie_scan;
     }
